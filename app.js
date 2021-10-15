3f4224aa3e172b8ef1542870bb02e233fe789dc8
VictorMayer <victor.mayer@outlook.com>
2021-10-14 23:18:55 -0300

    categories CRUD implemented

--- app.js
+++ app.js
@@ -24,17 +24,25 @@ const connection = new pg.Pool(connectionData);
 
 app.post("/categories", async (req, res) => {
     try {
-
+        const { name } = req.body;
+        const isNameAvailable = await connection.query("SELECT * FROM categories WHERE name = $1", [name]);
+        if(isNameAvailable.rows.length > 0) return res.sendStatus(409);
+        if(name.lenght === 0) return res.sendStatus(400);
+        await connection.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+        res.sendStatus(201);
     } catch (err) {
         console.log(err);
+        res.sendStatus(500);
     }
 });
 
 app.get("/categories", async (req, res) => {
     try {
-
+        const result = await connection.query(`SELECT * FROM categories`);
+        res.send(result.rows);
     } catch (err) {
         console.log(err);
+        res.sendStatus(500);
     }
 });
 
@@ -42,9 +50,11 @@ app.get("/categories", async (req, res) => {
 
 app.post("/games", async (req, res) => {
     try {
-
+        const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+        
     } catch (err) {
         console.log(err);
+        res.sendStatus(500);
     }
 });
 
@@ -127,5 +137,5 @@ app.delete("/rentals/:id", async (req, res) => {
 // turn server ON:
 
 app.listen(port, () => {
-    console.log("Server running on port: "+port);
+    console.log("Server running on port: "+chalk.blue(port));
 });
